a446b7bc6039c4115b394124fdc85eb44dc6b277
khadijaGo-star <76070898+khadijaGo-star@users.noreply.github.com>
2021-03-11 16:18:29 +0100

    Add files via upload

---
+++ Capstone_project.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# ### This notebook will be mainly used for the capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print(" Hello Capstone Project Course! ")
+
